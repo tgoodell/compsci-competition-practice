8ebcbfae9e140b7adfa2b70c91b16536c13ed7a3
tristan <me@tgoodell.com>
2020-01-29 14:14:45 -0600

    2

---
+++ ISBN10/tg_challenge_isbn10.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+import java.io.File; 
+import java.util.*;
+
+public class tg_challenge_isbn10 {
+	public static void main (String[] args) {
+        Scanner keyboard = new Scanner(System.in);
+        int x = keyboard.nextInt();
+        System.out.println(x);
+    }
+}
